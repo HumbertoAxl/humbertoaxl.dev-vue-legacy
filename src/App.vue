12fb16b8d9210be752360224247874fc4be55b63
Humberto Axl <humbertoaxl@gmail.com>
2023-01-30 00:08:31 +0800

    Placeholder for further developing

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,16 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+      <h1>Coming soon...</h1>
+      <img src="https://media.istockphoto.com/id/143921954/photo/male-chimpanzee-in-business-clothes.jpg?s=612x612&w=0&k=20&c=KoRUg4MNCZBsKgN9eZMqsknhO6UJGrhPH22OYOdqPlI=" class="logo" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <p>Powered by Monkey Corporation</p>
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
+  height: 25em;
   padding: 1.5em;
   will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
